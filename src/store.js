4440cc3c50c795d713d519ac5e5584f42dc02dbd
allen4dev <alanaliagadev@gmail.com>
2017-10-28 01:41:46 -0400

    [Add] redux devtools

--- src/store.js
+++ src/store.js
@@ -1,10 +1,19 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 
 import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer, applyMiddleware(logger, thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(logger, thunk),
+    typeof window === 'object' &&
+    typeof window.devToolsExtension !== 'undefined'
+      ? window.devToolsExtension()
+      : f => f
+  )
+);
 
 export default store;
